dc6977c7e941f04c3ebe94e7af9d65c4a0016148
Sama4444 <deprof4444@gmail.com>
2022-12-12 04:22:40 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
- * main - print a line using puts
+/**
+ * main -Print a line of code using puts
  *
  * Return: 0
 */
@@ -8,4 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}		
+}
