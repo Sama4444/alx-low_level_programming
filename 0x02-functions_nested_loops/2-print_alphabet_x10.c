a76ff67eab51e3d08fceaa81b2a570d631f85770
Sama4444 <deprof4444@gmail.com>
2022-12-14 00:28:43 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -11,7 +10,6 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('/n');
