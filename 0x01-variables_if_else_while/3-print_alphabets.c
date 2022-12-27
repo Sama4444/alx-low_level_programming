738e1150b793e2677fe164afc7351a812d9f7f89
Sama4444 <deprof4444@gmail.com>
2022-12-26 23:46:31 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z'){
+			putchar(c);
+			d++;
+		}
+	return (0);
+}
