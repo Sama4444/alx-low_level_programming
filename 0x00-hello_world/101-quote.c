38966b9a43bcc84dc36313dae5d418eba2ed3b72
Sama4444 <deprof4444@gmail.com>
2023-06-15 15:39:07 -0700

    quot

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
-#include <nistd.h>
+#include <unistd.h>
 
-/**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline
  * Return: 1.
  */
