88f63384c1f13c0e78d5e995cc7f938e4a9c3176
Sama4444 <deprof4444@gmail.com>
2023-06-15 05:38:55 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A C program whch print a line by using printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+	return : 0
+}
