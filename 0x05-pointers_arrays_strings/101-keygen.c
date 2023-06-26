1f654250c941efcc6db110070bc2f730c5b1a2be
Sama4444 <deprof4444@gmail.com>
2023-06-26 05:53:44 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -43,7 +43,7 @@ int main(void)
 				}
 					     }
 
-		                             for [index = 0; password[index]; index++)
+		                             for (index = 0; password[index]; index++)
 
 					     {
 						     if (password[index] >= (33 + diff_half2))
