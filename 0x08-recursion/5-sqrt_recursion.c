6402ca84b6e65cb6e29bac825d1d20a94eb81ebf
Sama4444 <deprof4444@gmail.com>
2023-07-05 06:13:53 -0700

    prime number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,18 +7,19 @@ int _sqrt(int n, int i);
  * _sqrt_recursion - function that returns the natural square root of a number.
  * @n: number to be calculated square root
  * @i: iterate number
- * Return: 0
+ * Return: the natural square root
  */
 int _sqrt_recursion(int n)
+
 {
 	return (_sqrt(n, 1));
 }
 
 /**
  * _sqrt - calculate natural square root
- * @n: number to be calculated the squrae root
+ * @n: number to be calculatei squrae root
  * @i: iterate number
- * Return: 0
+ * Return: the natural square root
  */
 int _sqrt(int n, int i)
 {
@@ -29,5 +30,6 @@ int _sqrt(int n, int i)
 
 	if (sqrt == n)
 		return (i);
+
 	return (_sqrt(n, i + 1));
 }
