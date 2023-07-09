cf6e4a2eccb9be7fac4c59ab44f41978566e2e72
Sama4444 <deprof4444@gmail.com>
2023-07-09 02:47:07 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,71 +1,28 @@
-#include <stdio.h>
 #include "main.h"
-
-/**
- * _atoi - convert a string to an integer
- * @s: coverted string
- * Return: the int that is converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '_')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program that multiplies two numbers
  * @argc: argurment number
  * @argv: number of array
- * Return: 0 (success) 1 (error)
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int n1 = 0, n2 = 0;
 
-	if (argc < 3 || argc > 3)
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
 
 	return (0);
 }
