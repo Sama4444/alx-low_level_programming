363ebb29ccba17e9b54b61fd1142b243703d3374
Sama4444 <deprof4444@gmail.com>
2023-07-17 14:22:58 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,11 @@ struct dog
 
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
