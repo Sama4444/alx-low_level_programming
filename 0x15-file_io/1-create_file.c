dbf7dcdaee9eb481a029d66dbe6230469fabeeea
Sama4444 <deprof4444@gmail.com>
2023-08-07 10:48:31 -0700

    creat_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,34 @@
 #include "main.h"
 
 /**
- * create_file - function that appends text at the end of a file
- * @filename: creat a file name to a pointer
- * @text_content: write a file to a string pointer
- * Return: if the function fails - -1 otherwise - 1
+ * create_file - function that creates a file
+ * @filename: creat a filename in a pointer
+ * @text_content: write a file that string a pointer
+ * Return: if the function fails - -1.
+ * otherwise - 1.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-        int fd, w, len = 0;
+	int fd, w, len = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (len = 0; text_content[len];)
-                        len++;
-        }
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        w = write(fd, text_content, len);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-        if (fd == -1 || w == -1)
-                return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-        close(fd);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
 
 	return (1);
 }
